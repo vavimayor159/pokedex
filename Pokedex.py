281ddef163af7937a3cf28d75f772bce3f270888
igarcia <ivan.garcia@crowdflower.com>
2020-05-27 15:48:03 -0500

    Reading json file example

--- Pokedex.py
+++ Pokedex.py
@@ -4,17 +4,17 @@ class Pokedex:
         print "Implement me"
 
 
-# We can read a JSON file using:
-#import json
-
-# read file
-#with open('example.json', 'r') as myfile:
-#    data=myfile.read()
-
-# parse file
-#obj = json.loads(data)
-
-# show values
-#print("usd: " + str(obj['usd']))
-#print("eur: " + str(obj['eur']))
-#print("gbp: " + str(obj['gbp']))
+# # We can read a JSON file using:
+# import json
+#
+# # read file
+# with open('example.json', 'r') as myfile:
+#     data=myfile.read()
+#
+# # parse file
+# obj = json.loads(data)
+#
+# # show values
+# print("usd: " + str(obj['usd']))
+# print("eur: " + str(obj['eur']))
+# print("gbp: " + str(obj['gbp']))
